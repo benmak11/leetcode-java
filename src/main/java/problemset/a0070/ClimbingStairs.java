ceb20ac8f5feda97929fdcebaa30419f8508c263
Ben Makusha <btmakusha@gmail.com>
2024-01-18 13:35:28 -0500

    Create 0070 Climbing Stairs problem

---
+++ src/main/java/problemset/a0070/ClimbingStairs.java
@@ -0,0 +1,13 @@
+package problemset.a0070;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int a = 0, b = 1;
+        for (int i = 0; i < n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}

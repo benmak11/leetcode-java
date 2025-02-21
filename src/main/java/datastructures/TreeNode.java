69c29ef5cc16ed8221ac5a162f6ae628e3ddbd89
Ben Makusha <btmakusha@gmail.com>
2025-02-21 11:47:01 -0500

    Established a build tree method

--- src/main/java/datastructures/TreeNode.java
+++ src/main/java/datastructures/TreeNode.java
@@ -1,5 +1,7 @@
 package datastructures;
 
+import java.util.*;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -15,5 +17,22 @@ public class TreeNode {
         this.val = _val;
         this.left = _left;
         this.right = _right;
+    }
+
+    public TreeNode buildTree(List<Integer> list) {
+        if (list.size() == 0)
+            return null;
+
+        return buildTreeHelper(list, 0);
+    }
+
+    private TreeNode buildTreeHelper(List<Integer> list, int index) {
+        if (index >= list.size())
+                return null;
+
+        TreeNode node = new TreeNode(list.get(index));
+        node.left = buildTreeHelper(list, 2 * index + 1);
+        node.right = buildTreeHelper(list, 2 * index + 2);
+        return node;
     }
 }
